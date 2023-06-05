ce794624403809798eaa2bcb4a28b5c8ca5d5c41
Кайдаров Павел <pavel.kaydarov@fabit.ru>
2023-06-05 13:56:50 +0300

    настроена сборка для различных окружений

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import {ads} from './test';
 
 console.log(ads(123));
+
+document.body.innerHTML = `<div>asdsaasdd</div>`
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,22 +1,23 @@
-import webpack from 'webpack';
 import {buildWebpackConfig} from "./config/build/buildWebpackConfig";
-import {BuildPaths} from "./config/build/types/config";
+import {BuildEnv, BuildPaths} from "./config/build/types/config";
 import path from "path";
 
-const paths: BuildPaths = {
-	entry: path.resolve(__dirname, 'src', 'index.ts'),
-	build: path.resolve(__dirname, 'build'),
-	html: path.resolve(__dirname, 'public', 'index.html'),
-}
 
-const mode = 'development';
+export default (env: BuildEnv) => {
+	const paths: BuildPaths = {
+		entry: path.resolve(__dirname, 'src', 'index.ts'),
+		build: path.resolve(__dirname, 'build'),
+		html: path.resolve(__dirname, 'public', 'index.html'),
+	}
 
-const isDev = mode === 'development'
+	const mode = env.mode || 'development';
+	const PORT = env.port || 3000;
+	const isDev = mode === 'development';
 
-const config: webpack.Configuration = buildWebpackConfig({
-	mode,
-	paths,
-	isDev,
-});
-
-export default config;
+	return buildWebpackConfig({
+		mode,
+		paths,
+		isDev,
+		port: PORT,
+	});
+};
