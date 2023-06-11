74e8353101c4fc8e6bf9e48844f6cdb3986777d8
Кайдаров Павел <pavel.kaydarov@fabit.ru>
2023-06-11 19:47:21 +0300

    react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import {Counter} from "./components/Counter";
+import React, {Suspense} from 'react';
+import {Link, Route, Routes} from 'react-router-dom';
+import MainPage from "./pages/MainPage/loader";
+import AboutPage from "./pages/AboutPage/loader";
 import './index.scss';
 
 export const App = () => {
     return (
         <div className="app">
-            asdsadsad
-            <Counter/>
+            <Link to='/'>Main</Link>
+            <Link to='/about'>About</Link>
+            <Suspense fallback={<>Loading...</>}>
+                <Routes>
+                    <Route path='/' element={<MainPage/>}/>
+                    <Route path='/about' element={<AboutPage/>}/>
+                </Routes>
+            </Suspense>
         </div>
     );
 };
