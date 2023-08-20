0410870a65ae969d0f3ec8bc87594c9e1ea2c3b7
Кайдаров Павел <pavel.kaydarov@fabit.ru>
2023-08-20 22:41:47 +0300

    ts fix

--- src/app/providers/ErrorBoundary/ui/ErrorBoundary.tsx
+++ src/app/providers/ErrorBoundary/ui/ErrorBoundary.tsx
@@ -18,7 +18,7 @@ class ErrorBoundary
 
     static getDerivedStateFromError(error: Error) {
         // Update state so the next render will show the fallback UI.
-        return { hasError: true };
+        return { hasError: true, error };
     }
 
     componentDidCatch(error: Error, errorInfo: ErrorInfo) {
