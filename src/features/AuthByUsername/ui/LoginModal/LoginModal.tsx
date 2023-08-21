4b28185de8856d76640503292ee28c84975c1897
Кайдаров Павел <pavel.kaydarov@fabit.ru>
2023-08-21 19:05:36 +0300

    add pre commit

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,19 +1,15 @@
-import { classNames } from 'shared/lib/classNames/classNames';
 import { Modal } from 'shared/ui/Modal/Modal';
-import cls from './LoginModal.module.scss';
 import { LoginForm } from '../LoginForm/LoginForm';
 
 interface ILoginModalProps {
-    className?: string;
     isOpen: boolean;
     onClose: () => void;
 }
 
 export const LoginModal = (props: ILoginModalProps) => {
-    const { className, isOpen, onClose } = props;
+    const { isOpen, onClose } = props;
     return (
         <Modal
-            className={classNames(cls.LoginModal, {}, [className])}
             isOpen={isOpen}
             onClose={onClose}
             lazy
