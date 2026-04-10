b4fa10bd7dd97ba2c53905642bc01a3217507930
Kaydarov Pavel <psse4ssp@gmail.com>
2026-04-10 09:20:23 +0300

    view by device

--- src/features/notifications/notificationButton/ui/NotificationButton/NotificationButton.tsx
+++ src/features/notifications/notificationButton/ui/NotificationButton/NotificationButton.tsx
@@ -3,17 +3,45 @@ import { AnchorPosition, Popover } from 'shared/ui/Popups';
 import NotificationIcon from 'shared/assets/icons/notification.svg';
 import { Icon } from 'shared/ui/Icon';
 import { NotificationsList } from 'entities/Notification';
+import { Drawer } from 'shared/ui/Drawer';
+import { useCallback, useState } from 'react';
+import { BrowserView, MobileView } from 'react-device-detect';
 import cls from './NotificationButton.module.scss';
 
-export const NotificationButton = () => (
-    <Popover
-        buttonContent={(
-            <Button theme={ButtonTheme.CLEAR}>
-                <Icon Svg={NotificationIcon} inverted />
-            </Button>
-        )}
-        anchor={AnchorPosition.TOP_END}
-    >
-        <NotificationsList className={cls.notifications} />
-    </Popover>
-);
+export const NotificationButton = () => {
+    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+    const onOpenDrawer = useCallback(() => {
+        setIsDrawerOpen(true);
+    }, []);
+
+    const onCloseDrawer = useCallback(() => {
+        setIsDrawerOpen(false);
+    }, []);
+
+    const trigger = (
+        <Button theme={ButtonTheme.CLEAR} onClick={onOpenDrawer}>
+            <Icon Svg={NotificationIcon} inverted />
+        </Button>
+    );
+
+    return (
+        <div>
+            <BrowserView>
+                <Popover
+                    buttonContent={trigger}
+                    anchor={AnchorPosition.TOP_END}
+                >
+                    <NotificationsList className={cls.notifications} />
+                </Popover>
+            </BrowserView>
+            <MobileView>
+                {trigger}
+                <Drawer isOpen={isDrawerOpen} onClose={onCloseDrawer} className={cls.drawer}>
+                    <NotificationsList />
+                </Drawer>
+            </MobileView>
+        </div>
+
+    );
+};
